f2b8164ba75eca866832e74d5dfb7434c499d3e7
manhtranduc <manhtranduc0202@gmail.com>
2025-12-13 22:35:03 +0700

    first commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,4 @@
+import { DevoverUi } from '@common/ui/ui';
+export default function Index() {
+  return <DevoverUi />;
+}
---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,9 @@
+export function DevoverUi() {
+  return (
+    <div>
+      <h1>Welcome to DevoverUi!</h1>
+    </div>
+  );
+}
+
+export default DevoverUi;
